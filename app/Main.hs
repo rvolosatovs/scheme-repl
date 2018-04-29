b1d2e80626a41987dd13edc2d605723c9992af73
Roman Volosatovs <rvolosatovs@riseup.net>
2018-04-29 16:52:43 +0200

    exe,lib: Implement repl/env, refactor
    - Remove unhelpful comments
    - Move all parsing/repl logic to one file/package
    - Ensure definitions are specified in "top-to-bottom" fashion
    - Implement REPL
    - Implement environment

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
-import Lib
+import           Lisp               (runOne, runRepl)
+import           System.Environment
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case length args of
+    0         -> runRepl "Lisp λ"
+    1         -> runOne $ args !! 0
+    otherwise -> putStrLn "Program takes at most 1 argument"
